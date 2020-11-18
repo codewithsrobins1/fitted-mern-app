3b09a3f8faa2f1d13c92ec1340d7d4689971a625
unknown <codewithsrobins1@gmail.com>
2020-11-18 14:26:35 -0800

    installed material ui package and created post, posts, and form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import fitted from './images/fitted-logo.png';
 
 const App = () => {
     return(
-        <div>
-            <h1>Fitted</h1>
-        </div>
+        <Container maxwidth='lg'>
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Fitted</Typography>
+                <img src={fitted} alt="outfits" height="60" /> 
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
