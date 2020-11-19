0590f977c371692442cc5ab5194d50a1aa2872a7
unknown <codewithsrobins1@gmail.com>
2020-11-18 22:59:33 -0800

    setup form for posting fits

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { getPosts } from './actions/posts';
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import fitted from './images/fitted-logo.png';
@@ -7,6 +9,11 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return(
         <Container maxwidth='lg'>
             <AppBar className={classes.appBar} position="static" color="inherit">
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyles from './styles';
+import FileBase from 'react-file-base64';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        comment: '',
+        tags: '',
+        selectedFile: ''
+    });
     const classes = useStyles();
+    const handleSubmit = () => {
+
+    }
+    const clear = () => {
+
+    }
+
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">Create a Fit</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator}
+                    onChange={(e) => setPostData({
+                        ...postData,                //spread post data, otherwise creator only gets updated, other data can persist
+                        creator: e.target.value
+                    })}
+                />
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title}
+                    onChange={(e) => setPostData({
+                        ...postData,                //spread post data, otherwise title only gets updated, other data can persist
+                        title: e.target.value
+                    })}
+                />
+                <TextField name="comment" variant="outlined" label="Comment" fullWidth value={postData.comment}
+                    onChange={(e) => setPostData({
+                        ...postData,                //spread post data, otherwise comment only gets updated, other data can persist
+                        comment: e.target.value
+                    })}
+                />
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} 
+                    onChange={(e) => setPostData({
+                        ...postData,                //spread post data, otherwise tags only gets updated, other data can persist
+                        tags: e.target.value
+                    })}
+                />
+            <div className={classes.fileInput}>
+                <FileBase type="file"multiple={false}
+                    onDone={({base64}) => setPostData({
+                        ...postData,
+                        selectedFile: base64
+                    })}    
+                />
+            </div>
+            <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>
+                Submit
+            </Button>
+            <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>
+                Clear
+            </Button>
+            </form>
+        </Paper>
     )
 }
 
