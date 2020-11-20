ec129aa33788bc90b5f639c89e9b975d82adc377
unknown <codewithsrobins1@gmail.com>
2020-11-20 15:59:54 -0800

    create delete route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 
@@ -36,10 +37,25 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body;
 
+    //Valid ID?
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
     //Get access to the updated post
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
 
     res.json(updatedPost);
+}
+
+//DELETE POST
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    //Valid ID?
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({
+        message: 'Post deleted successfully'
+    });
 }
