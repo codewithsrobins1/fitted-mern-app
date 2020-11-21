ce24f385b9a0a71347d61575624c234b186a5446
unknown <codewithsrobins1@gmail.com>
2020-11-20 17:15:13 -0800

    added env file for backend security

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 const app = express();
+dotenv.config();
 
 //Parse Files
 app.use(bodyParser.json({
@@ -19,13 +21,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-
-
 //Connect to Mongo DB
-const MONGO_CONNECTION_URL = 'mongodb+srv://fitted-admin:test123@bloggerapplication.hv64k.mongodb.net/FittedApp?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGO_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) =>  console.log(err.message));
 
