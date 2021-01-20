9d9b011926bffada67438610f799443866b7f970
unknown <codewithsrobins1@gmail.com>
2021-01-19 16:53:54 -0800

    adding files

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -18,7 +18,11 @@ export const getPosts = async (req, res) => {
 //CREATE A POST
 export const createPost = async (req, res) => {
     const post = req.body;
-    const newPost = new PostMessage(post);
+    const newPost = new PostMessage({ 
+        ...post, 
+        creator: req.userId, 
+        createdAt: new Date().toISOString()
+    });   
     
     try {
         await newPost.save();
@@ -64,11 +68,28 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
+    if(!req.userId) return res.json({ message: 'Unauthenticated '});
+
     //Valid ID?
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true});
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    //-1 if the id is not in the index
+    if(index === -1){
+        //Users wants to like the post
+        post.likes.push(req.userId)
+    }
+    else{
+        //dislike a post
+        post.likes = post.likes.filter((id) => {
+            id !== String(req.userId)
+        })
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true});
 
     res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 const app = express();
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Fitted-App API');
