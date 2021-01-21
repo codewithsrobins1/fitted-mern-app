dfeab085b9a8c863d8a5c3b42eaf47b4992d8b4d
unknown <codewithsrobins1@gmail.com>
2021-01-20 22:15:53 -0800

    added toast notification for successful updates and edits

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import useStyles from './styles';
 import FileBase from 'react-file-base64';
 import { createPost, updatePost } from '../../actions/posts';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
@@ -37,15 +39,42 @@ const Form = ({ currentId, setCurrentId }) => {
         //If there is an id, updating. Otherwise create a post
         if(currentId === 0){
             dispatch(createPost({ ...postData, name: user?.result?.name }))
+            toastTypeMsg('success', 'Your fit has been successfully created.');
             clear();
         }
         else{
             dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
+            toastTypeMsg('success', 'You have updated your fit.');
             clear();
         }
     }
 
-    
+    const toastTypeMsg = (type, msg) => {
+        switch(type){
+            case 'error':
+              return toast.error(`${msg}`, {
+                position: "bottom-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
+              });
+            case 'success':
+              return toast.success(`${msg}`, {
+                position: "bottom-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined
+              })
+            default:
+                break;
+            }
+    }
 
     if(!user?.result?.name){
         return(
@@ -58,43 +87,56 @@ const Form = ({ currentId, setCurrentId }) => {
     }
 
     return (
-        <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant="h6">{currentId ? 'Editing a Fit' : 'Create a Fit'}</Typography>
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title}
-                    onChange={(e) => setPostData({
-                        ...postData,                //spread post data, otherwise creator only gets updated, other data can persist
-                        title: e.target.value
-                    })}
-                />
-                <TextField name="comment" variant="outlined" label="Comment" fullWidth value={postData.comment}
-                    onChange={(e) => setPostData({
-                        ...postData,                
-                        comment: e.target.value
-                    })}
-                />
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} 
-                    onChange={(e) => setPostData({
-                        ...postData,                
-                        tags: e.target.value.split(',')
-                    })}
-                />
-            <div className={classes.fileInput}>
-                <FileBase type="file" multiple={false}
-                    onDone={({base64}) => setPostData({
-                        ...postData,
-                        selectedFile: base64
-                    })}    
-                />
-            </div>
-            <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>
-                Submit
-            </Button>
-            <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>
-                Clear
-            </Button>
-            </form>
-        </Paper>
+        <div>
+            <Paper className={classes.paper}>
+                <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                    <Typography variant="h6">{currentId ? 'Editing a Fit' : 'Create a Fit'}</Typography>
+                        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title}
+                            onChange={(e) => setPostData({
+                                ...postData,                //spread post data, otherwise creator only gets updated, other data can persist
+                                title: e.target.value
+                            })}
+                        />
+                        <TextField name="comment" variant="outlined" label="Comment" fullWidth value={postData.comment}
+                            onChange={(e) => setPostData({
+                                ...postData,                
+                                comment: e.target.value
+                            })}
+                        />
+                        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} 
+                            onChange={(e) => setPostData({
+                                ...postData,                
+                                tags: e.target.value.split(',')
+                            })}
+                        />
+                    <div className={classes.fileInput}>
+                        <FileBase type="file" multiple={false}
+                            onDone={({base64}) => setPostData({
+                                ...postData,
+                                selectedFile: base64
+                            })}    
+                        />
+                    </div>
+                    <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>
+                        Submit
+                    </Button>
+                    <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>
+                        Clear
+                    </Button>
+                </form>
+            </Paper>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </div>
     )
 }
 
